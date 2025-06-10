794b33b2973701764ee5c64d985dfc6928b38475
Glitched1221 <leostevens008@gmail.com>
2025-06-10 16:09:19 +0100

    Movement
    We did it boys after 5 Hours Basic Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+      private Vector3 moveDirection;
+      public float horizontalInput;
+      public int moveSpeed = 10;
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(-Vector3.right*Time.deltaTime * horizontalInput* moveSpeed) ;
+
+
+    }
+     
+
+    
+}
