9757dccabb0ab45506fd46d7aaec3e02d4b1912c
Heather Beaumont <heathbeay@gmail.com>
2025-07-02 15:11:37 +0100

    i think the oven works?

--- Assets/Scripts/Interactable/Oven.cs
+++ Assets/Scripts/Interactable/Oven.cs
@@ -5,17 +5,30 @@ using UnityEditor;
 using UnityEngine;
 public class Oven : Interactable
 {
+    private bool firstStageComplete = false;
     public override void Interact(Collider col)
     {
-        if(Input.GetKey(KeyCode.E))
+        if(Input.GetKey(KeyCode.E) & PlayerHolding.currentlyHeldItem == empty)
         {
+            PlayerHolding.currentlyHeldItem = (internalItems[internalItems.Count - 1] != null) ? internalItems[internalItems.Count - 1] : empty;
+            if(timerActive)
+            {
+                timerActive = false;
+                firstStageComplete = false;
+            }
         }
     }
     void LateUpdate()
     {
         if(!timerActive & ValidateRecipeBool())
         {
-            
+            if(firstStageComplete)
+            {
+                Item tmp = ValidateRecipe();
+                internalItems.Clear();
+                internalItems.Add(tmp);
+            }
         }
+        if(timerActive && !firstStageComplete) firstStageComplete = true;
     }
 }
